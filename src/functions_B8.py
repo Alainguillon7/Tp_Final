3a3e12f96bcf5a7e7eb14e67c064e809b782dd3a
Alain Guillon <alainguillon01@gmail.com>
2025-04-24 17:03:33 -0300

    Repeticion de codigo E8

--- src/functions_B8.py
+++ src/functions_B8.py
@@ -1,11 +1,12 @@
 from utils import get_indice
 
 
-def porcentaje (filas):
+def porcentaje(filas):
     values_region = {}
 
-    region = get_indice(filas[0],"REGION")
-    ii7 = get_indice(filas[0],"II7")
+    region = get_indice(filas[0], "REGION")
+    ii7 = get_indice(filas[0], "II7")
+    i_cant = get_indice(filas[0], "PONDERA")
 
     for fila in filas[1:]:
 
@@ -14,16 +15,14 @@ def porcentaje (filas):
             values_region[clave] = {"total": 0, "inquilinos": 0}
 
         if fila[ii7] == "3":
-            values_region[clave]["inquilinos"] += 1
-            values_region[clave]["total"] += 1
-        else:
-            values_region[clave]["total"] += 1
+            values_region[clave]["inquilinos"] += fila[i_cant]
+        values_region[clave]["total"] += fila[i_cant]
 
     porcentaje_region = {}
 
     for region, datos in values_region.items():
-        total = datos['total']
-        inquilinos = datos['inquilinos']        
-        porcentaje_region[region] = (inquilinos / total) * 100 
+        total = datos["total"]
+        inquilinos = datos["inquilinos"]
+        porcentaje_region[region] = (inquilinos / total) * 100
 
     return porcentaje_region
